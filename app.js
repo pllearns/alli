0c5ec8d3b20d6e5b770a08c86226a3ad4c0c6e3f
pllearns <pllearns@gmail.com>
2017-11-12 15:35:22 -0800

    Update dependency import

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const receivedMessage = require('./helpers/helpers')
 
 const port = process.env.PORT || 3000
 
