542077317b757564b86d87d6bfecbdd26441ba79
pllearns <pllearns@gmail.com>
2017-11-12 11:53:24 -0800

    Clean up error in app.js

--- app.js
+++ app.js
@@ -22,32 +22,8 @@ app.get('/webhook', (req, res) => {
   }
 })
 
-app.post('/webhook', (req, res) => {
-  let data = req.body
-
-  if (data.object === 'page') {
-    data.entry.forEach((entry) => {
-      let pageID = entry.id
-      let timeOfEvent = entry.time
-
-      entry.messaging.forEach((event) => {
-        if (event.message) {
-          receivedMessage(event)
-        } else {
-          console.log('Webhook received unknown event', event)
-        }
-      })
-    })
-
-    res.sendStatus(200)
-  }
-})
-
 app.listen(port, () => {
   console.log(`You are active on http://localhost:${port}`)
 })
 
-app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
-})
 
